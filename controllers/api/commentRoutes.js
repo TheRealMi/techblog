e9229ed1e599556eba37ad33624148d396d3c59e
The Real Mi <miaolmedillo@gmail.com>
2023-10-05 12:14:41 -0400

    added logged in condition to add comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,16 +4,22 @@ const withAuth = require('../../utils/auth');
 
 // Create a new comment
 router.post('/', withAuth, async (req, res) => {
-    try {
-        const newComment = await Comment.create({
-            ...req.body,
-            user_id: req.session.user_id
-        });
-
-        res.status(200).json(newComment);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+    if (req.session.logged_in) {
+        try {
+            const newComment = await Comment.create({
+                ...req.body,
+                user_id: req.session.user_id
+            });
+    
+            res.status(200).json(newComment);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+        return;
+      }
+    
+      res.render('login');
+    
 });
 
 module.exports = router;
